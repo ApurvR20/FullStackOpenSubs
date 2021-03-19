bbcbcb22495399fe3f05747c4e57ae283940ddaf
ApurvR20 <62154933+ApurvR20@users.noreply.github.com>
2021-03-20 01:49:03 +0530

    Add files via upload

---
+++ Part 1/Exercise 1.1/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Header = (props) =>
+{
+  return(
+    <div>
+      <p>
+        Course = {props.name}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) =>
+{
+  return(
+    <div>
+      <p>
+        Part 1 : {props.part1} Exercises = {props.exercises1}
+        <br/>
+        Part 2 : {props.part2} Exercises = {props.exercises2}
+        <br/>
+        Part 3 : {props.part3} Exercises = {props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) =>{
+  return(
+    <div>
+      <p>
+      Total no. of exercises = {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name = {course} />
+      <Content part1 = {part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      {
+      console.log('Woof')
+    }
+    </div>
+  )
+}
+
+export default App
---
+++ Part 1/Exercise 1.2/src/App.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const Header = (props) =>
+{
+  return(
+    <div>
+      <p>
+        Course = {props.name}
+      </p>
+    </div>
+  )
+}
+
+const Part = (props) =>{
+  return(
+    <div>
+      <p>
+        Part {props.number} : {props.part} Exercises = {props.exercise}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) =>
+{
+  return(
+    <div>
+      <Part number={1} part={props.part1} exercise={props.exercises1}  />
+      <Part number={2} part={props.part2} exercise={props.exercises2}  />
+      <Part number={3} part={props.part3} exercise={props.exercises3}  />
+    </div>
+  )
+}
+
+
+
+const Total = (props) =>{
+  return(
+    <div>
+      <p>
+      Total no. of exercises = {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name = {course} />
+      <Content part1 = {part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      {
+      console.log('mew')
+      }
+    </div>
+  )
+}
+
+export default App
